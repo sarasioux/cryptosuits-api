3667a8b1da3e0d69ee4833b2dcd6f1071ac60892
Sara Robertson <sara.melodramatic@gmail.com>
2021-11-23 11:04:35 -0500

    convert png to svg to png

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const Web3 = require('web3');
 const TruffleContract = require("@truffle/contract");
 const fs = require('fs');
 
+const { convert } = require('convert-svg-to-png');
+
 // Constants
-const siteUrl = 'https://cryptosuits.netlify.app/';
+const siteUrl = 'https://cryptosuits.herokuapp.com/';
+//const siteUrl = 'http://localhost:3000/';
 const provider = 'wss://rinkeby.infura.io/ws/v3/721b7c03aa4d4431b4d6a5f9876d116a';
 //const provider = 'http://127.0.0.1:7545';
 const ownerAccount = '0x00796e910Bd0228ddF4cd79e3f353871a61C351C';                  // Address of the current queryer
@@ -29,6 +32,8 @@ router.get('/json/:id', function(req, res) {
                 if(reveal) {
                   const data = fs.readFileSync('./tokens/output/' + id + '.json');
                   const json = JSON.parse(data);
+                  json.ipfs = json.image;
+                  json.image = siteUrl + 'image/' + id;
                   res.json(json);
                 } else {
                   res.json({image: siteUrl + 'suit.png'});
@@ -57,6 +62,17 @@ router.get('/json2/:id', function(req, res) {
       //}
     //});
   //});
+});
+
+router.get('/image/:id', function(req, res) {
+  const id = req.params.id;
+  const data = fs.readFileSync('./tokens/output/' + id + '.json');
+  const json = JSON.parse(data);
+  let svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"><image href="${json.image}" height="1000" width="1000" style="image-rendering: pixelated" /></svg>`;
+  convert(svg, {width: 1000, height: 1000, puppeteer: { timeout: 0, args: ['--no-sandbox', '--disable-setuid-sandbox']}}).then((png) => {
+    res.set('Content-Type', 'image/png');
+    res.send(png);
+  });
 });
 
 router.get('/CryptoSuits.json', function(req, res) {
